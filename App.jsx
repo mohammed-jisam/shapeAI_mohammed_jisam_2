eb939717a1018daa5cbc858c783cea8594013e73
mohammed-jisam <83239441+mohammed-jisam@users.noreply.github.com>
2021-06-06 16:13:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from  'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx'
+import Notes from './Notes.jsx'
+
+function App(){
+    return(
+        <div>
+            <Header/>
+            <Notes/>
+            <Notes/>
+            <Notes/>
+            <Notes/>
+            <Footer/>
+
+        </div>
+    )
+}
+export default App;
---
+++ Notes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(){
+    
+    return(
+       <div className="note">
+           <h1>JavaScript and React.js</h1>
+           <p>
+               This was an amazing Bootcamp taken by Saurya sir. He convey all topic very easy and we covered everything from scratch including JAvaScript
+           </p>
+       </div>
+      
+    )
+}
+export default Footer;
